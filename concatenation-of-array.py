bd694f053970e6c4d4e17b71fddc2322c78b1573
Eilaf649 <107715838+Eilaf649@users.noreply.github.com>
2026-02-05 14:17:13 +0800

    Create concatenation-of-array.py

---
+++ concatenation-of-array.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums1=nums
+        result=nums+nums1
+        return result
