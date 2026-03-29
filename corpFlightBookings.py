96b805ea1e88ab2e0fe560f873ad073b1eb5ef61
Eilaf649 <107715838+Eilaf649@users.noreply.github.com>
2026-03-29 20:40:56 +0800

    Create corpFlightBookings.py

---
+++ corpFlightBookings.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def corpFlightBookings(self, bookings, n):
+        """
+        :type bookings: List[List[int]]
+        :type n: int
+        :rtype: List[int]
+        """
+        res=[0]*(n+1)
+        for i,j,k in bookings:
+            res[i-1]+=k
+            res[j]-=k
+        for i in xrange(1,n):
+            res[i]+=res[i-1]
+        return res[:-1]
