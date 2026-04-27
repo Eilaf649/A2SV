f1b6c3d6837e7cb9a3b17d57689b8ebb042618c0
Eilaf649 <107715838+Eilaf649@users.noreply.github.com>
2026-04-28 00:04:38 +0800

    Create arrangeCoins.py

---
+++ arrangeCoins.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def arrangeCoins(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left=1
+        right=n
+        while left<=right:
+            mid=(left+right)//2
+            if (mid*(mid+1))//2==n:
+                return mid
+            elif (mid*(mid+1))//2>n:
+                right=mid-1
+            else:
+                left=mid+1
+        return left-1
+        
