cc2d95ce229af945ec0ae8b80f0456319cb6a136
Eilaf649 <107715838+Eilaf649@users.noreply.github.com>
2026-04-28 22:39:33 +0800

    Create inorderTraversal.py

---
+++ inorderTraversal.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def rec(self, root):
+        if root:
+            self.rec(root.left)
+            self.res.append(root.val)
+            self.rec(root.right)
+
+    def inorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        self.res=[]
+        self.rec(root)
+        return self.res
+        
