31344242bad3164f0936b95a2be11020d70efe67
Eilaf649 <107715838+Eilaf649@users.noreply.github.com>
2026-04-29 21:23:27 +0800

    Create postorderTraversal.py

---
+++ postorderTraversal.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def rec(self, root):
+        if root:
+            self.rec(root.left)
+            self.rec(root.right)
+            self.res.append(root.val)
+    def postorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        self.res=[]
+        self.rec(root)
+        return self.res
+        
+        
